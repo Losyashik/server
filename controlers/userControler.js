e43d623848165e78c7c4889332777aa0e9462cfc
Anton <An.Shkenin@yandex.ru>
2022-12-03 07:39:24 +0300

    Update userControler.js

--- controlers/userControler.js
+++ controlers/userControler.js
@@ -2,7 +2,7 @@ import conn from "./../database/connect.js";
 
 class userControler {
   async getAllUsers(req, res) {
-    const result = await conn.query("INSERT INTO users (id, name) VALUES (1, 'Иванов И. И.'),(2, 'Петров П. П.'),(3, 'Сидоров С. С.');");
+    const result = await conn.query("SELECT * FROM users");
     res.json(result.rows);
   }
 }
